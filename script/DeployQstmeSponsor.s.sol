edcf36d673df4152a2809e76af82b06edf63480a
questme-official <tech@qstme.io>
2025-04-07 00:21:56 +0300

    created helper for safe singleton deployment, rewrited deployment scripts to use it

--- script/DeployQstmeSponsor.s.sol
+++ script/DeployQstmeSponsor.s.sol
@@ -2,27 +2,34 @@
 pragma solidity ^0.8.24;
 
 import "lib/forge-std/src/Script.sol";
+import {SafeSingletonDeployer} from "./helpers/SafeSingletonDeployer.sol";
 
 import {QstmeSponsor} from "src/QstmeSponsor.sol";
 
 contract DeployQstmeSponsorScript is Script {
+    address public constant ADMIN = 0x885CefFc2f5428C3A2C5895204335ED6dcf466a1;
+    address public constant OPERATOR = 0x91430EC444FD8249e152aDf82a73f985b031276E;
+
     function run(string calldata network) external {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_KEY");
+        bytes32 salt = vm.envBytes32("SAFE_SINGLETON_SALT");
 
-        _run(network, deployerPrivateKey);
+        _run(network, deployerPrivateKey, salt);
     }
 
-    function _run(string calldata network, uint256 deployerPrivateKey) internal {
+    function _run(string calldata network, uint256 deployerPrivateKey, bytes32 salt) internal {
         vm.createSelectFork(network);
 
         address deployer = vm.addr(deployerPrivateKey);
 
-        vm.broadcast(deployerPrivateKey);
-        QstmeSponsor qstMeSponsor = new QstmeSponsor(
-            deployer,
-            0x381c031bAA5995D0Cc52386508050Ac947780815
-        );
-
-        console.log("Sponsor contract deployed at: ", address(qstMeSponsor));
+        address qstmeSponsor = SafeSingletonDeployer.broadcastDeploy({
+            deployerPrivateKey: deployerPrivateKey,
+            creationCode: type(QstmeSponsor).creationCode,
+            args: abi.encode(
+                ADMIN,
+                OPERATOR
+            ),
+            salt: salt
+        });
     }
 }
